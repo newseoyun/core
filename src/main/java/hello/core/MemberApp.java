3182d55e01e6e33b44845db83cd1a186b32b635c
newseoyun <newseoyun@gmail.com>
2021-04-29 10:58:02 +0900

    .

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,9 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
+
         MemberService memberService = new MemberServiceImpl();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
