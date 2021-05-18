0efe6ab2fde4ce82b77dab7a89c90cea55978c05
macSeoyun <newseoyun@gmail.com>
2021-05-18 19:52:00 +0900

    .

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        /*basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+         디폴트 값은 이 설정파일의 위치.
+         메인 메소드의 @SpringBootApplication 여기 안에 @ComponentScan 이 들어있기 때문에
+         스프링부트 애플리케이션을 생성하면 자동으로 디폴트 경로가 지정되서 등록이 됨. */
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
