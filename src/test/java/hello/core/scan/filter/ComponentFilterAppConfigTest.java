667ee2394b43822412d9e29ce31fd69819f82172
macSeoyun <newseoyun@gmail.com>
2021-05-19 23:27:45 +0900

    .

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.context.annotation.ComponentScan.Filter;
@@ -27,8 +26,8 @@ public class ComponentFilterAppConfigTest {
 
     @Configuration
     @ComponentScan(
-            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
-            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+            includeFilters = @Filter(classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(classes = MyExcludeComponent.class)
     )
     static class ComponentFilterAppConfig {
 
