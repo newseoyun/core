dc9ff11d02c4960907d81f4817b7f42a2d631abe
macSeoyun <newseoyun@gmail.com>
2021-05-23 23:01:51 +0900

    .

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,36 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false) // 자동주입 대상이 없으면 메서드 자체가 호출되지 않음.
+        public void setNoNean1(Member noNean1) {
+            System.out.println("noNean1 = " + noNean1);
+        }
+
+        @Autowired
+        public void setNoNean2(@Nullable Member noNean2) {
+            System.out.println("noNean2 = " + noNean2);
+        }
+
+        @Autowired
+        public void setNoNean3(Optional<Member> noNean3) {
+            System.out.println("noNean3 = " + noNean3);
+        }
+    }
+}
--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -1,12 +1,11 @@
 package hello.core.beanfind;
 
-import hello.core.AppConfig;
-import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +17,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextSameBeanFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+    ApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
     @Test
     @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
     void findBeanByTypeDuplicate() {
-        MemberRepository bean = ac.getBean(MemberRepository.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(MemberRepository.class));
     }
