47a1656b394ff5ffb24724beca3191de250988f4
macSeoyun <newseoyun@gmail.com>
2021-06-02 20:21:37 +0900

    .

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -16,15 +17,30 @@ public class NetworkClient {
 
     // 서비스 시작 시 호출
     public void connect() {
-        System.out.println("connect = " + url);
+        System.out.println("connect: " + url);
     }
 
     public void call(String message) {
-        System.out.println("connect = " + url + " message = " + message);
+        System.out.println("call:" + url + " message: " + message);
     }
 
     // 서비스 종료 시 호출
     public void disconnect() {
-        System.out.println("close = " + url);
+        System.out.println("close: " + url);
+    }
+
+
+    // InitializingBean, DisposableBean : 초기화, 소멸 인터페이스. 잘 안씀.
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
