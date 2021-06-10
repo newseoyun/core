51888a7c93a7ec9321c6e46e224cb0d80edea97d
macSeoyun <newseoyun@gmail.com>
2021-06-11 00:03:56 +0900

    .

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,10 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+
+    /*
     private final ObjectProvider<MyLogger> myLoggerProvider;
     // MyLogger 의 scope 가 request 이므로  http 요청이 있을 때 싱글턴 빈을 생성하므로,
     // 기동 시엔 http 요청이 있을 수 없으므로 state 오류남.
     // 그래서 ObjectProvider 로 감싸줌 (호출 할 때까지 빈 생성 지연 기능)
+    // 프록시 객체를 생성해서 주입되도록 하는 방식을 사용하면 ObjectProvider 안써도 되고 더 깔끔함
 
     @RequestMapping("log-demo")
     @ResponseBody
@@ -33,5 +36,23 @@ public class LogDemoController {
         return "OK";
     }
 
+    */
+
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger.getClass() >>>" + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+
+        logDemoService.logic("testId");
+        return "OK";
+    }
 
 }
