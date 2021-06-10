9bc2c7aaf4868cb1f3214c357433d2285c4d12af
macSeoyun <newseoyun@gmail.com>
2021-06-10 23:45:54 +0900

    .

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -15,17 +15,19 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     private final ObjectProvider<MyLogger> myLoggerProvider;
-    // MyLogger 의 scope 가 request 이므로  http 요청이 있을 때 비로소 싱글턴 빈을 생성하므로 기동 시 state 오류남.
-    // 그래서 ObjectProvider 로 감싸줌
+    // MyLogger 의 scope 가 request 이므로  http 요청이 있을 때 싱글턴 빈을 생성하므로,
+    // 기동 시엔 http 요청이 있을 수 없으므로 state 오류남.
+    // 그래서 ObjectProvider 로 감싸줌 (호출 할 때까지 빈 생성 지연 기능)
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
         MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
 
         logDemoService.logic("testId");
         return "OK";
